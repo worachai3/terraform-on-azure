29f79191793f202f5c27458d49ec86820d7a8610
benzWRC <worachai3@live.com>
2023-08-23 23:39:31 +0700

    Add backend-state

--- main.tf
+++ main.tf
@@ -1,25 +1,25 @@
 resource "azurerm_resource_group" "appgrp" {
   name     = local.resource_group_name
-  location = local.location
+  location = local.location  
 }
 
 resource "azurerm_virtual_network" "appnetwork" {
   name                = local.virtual_network.name
   location            = local.location
   resource_group_name = local.resource_group_name
-  address_space       = [local.virtual_network.address_space]
+  address_space       = [local.virtual_network.address_space]  
+  
+   depends_on = [
+     azurerm_resource_group.appgrp
+   ]
+  }
 
-  depends_on = [
-    azurerm_resource_group.appgrp
-  ]
-}
 
-resource "azurerm_subnet" "subnetA" {
+  resource "azurerm_subnet" "subnetA" {
   name                 = local.subnets[0].name
   resource_group_name  = local.resource_group_name
-  virtual_network_name = azurerm_virtual_network.appnetwork.name
+  virtual_network_name = local.virtual_network.name
   address_prefixes     = [local.subnets[0].address_prefix]
-
   depends_on = [
     azurerm_virtual_network.appnetwork
   ]
@@ -28,16 +28,15 @@ resource "azurerm_subnet" "subnetA" {
 resource "azurerm_subnet" "subnetB" {
   name                 = local.subnets[1].name
   resource_group_name  = local.resource_group_name
-  virtual_network_name = azurerm_virtual_network.appnetwork.name
+  virtual_network_name = local.virtual_network.name
   address_prefixes     = [local.subnets[1].address_prefix]
-
   depends_on = [
     azurerm_virtual_network.appnetwork
   ]
 }
 
 resource "azurerm_network_interface" "appinterface" {
-  name                = "app-interface"
+  name                = "appinterface"
   location            = local.location
   resource_group_name = local.resource_group_name
 
@@ -45,10 +44,8 @@ resource "azurerm_network_interface" "appinterface" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnetA.id
     private_ip_address_allocation = "Dynamic"
-
     public_ip_address_id = azurerm_public_ip.appip.id
   }
-
   depends_on = [
     azurerm_subnet.subnetA
   ]
@@ -56,13 +53,12 @@ resource "azurerm_network_interface" "appinterface" {
 
 resource "azurerm_public_ip" "appip" {
   name                = "app-ip"
-  location            = local.location
   resource_group_name = local.resource_group_name
+  location            = local.location
   allocation_method   = "Static"
-
-  depends_on = [
-    azurerm_resource_group.appgrp
-  ]
+ depends_on = [
+   azurerm_resource_group.appgrp
+ ]
 }
 
 resource "azurerm_network_security_group" "appnsg" {
@@ -87,27 +83,24 @@ resource "azurerm_network_security_group" "appnsg" {
   ]
 }
 
-resource "azurerm_subnet_network_security_group_association" "appnsgassociation" {
+resource "azurerm_subnet_network_security_group_association" "appnsglink" {
   subnet_id                 = azurerm_subnet.subnetA.id
   network_security_group_id = azurerm_network_security_group.appnsg.id
-
-  depends_on = [
-    azurerm_subnet.subnetA,
-    azurerm_network_security_group.appnsg
-  ]
 }
 
 resource "azurerm_windows_virtual_machine" "appvm" {
-  name                  = "app-vm"
-  resource_group_name   = local.resource_group_name
-  location              = local.location
-  size                  = "Standard_D2S_v3"
-  admin_username        = "adminuser"
-  admin_password        = "Azure@123"
-  network_interface_ids = [azurerm_network_interface.appinterface.id]
+  name                = "appvm"
+  resource_group_name = local.resource_group_name
+  location            = local.location
+  size                = "Standard_D2s_v3"
+  admin_username      = "adminuser"
+  admin_password      = "Azure@123"
+  network_interface_ids = [
+    azurerm_network_interface.appinterface.id,
+    azurerm_network_interface.secondaryinterface.id
+  ]
 
   os_disk {
-    name                 = "app-osdisk"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
@@ -118,9 +111,48 @@ resource "azurerm_windows_virtual_machine" "appvm" {
     sku       = "2019-Datacenter"
     version   = "latest"
   }
-
   depends_on = [
     azurerm_network_interface.appinterface,
+    azurerm_resource_group.appgrp,
+    azurerm_network_interface.secondaryinterface
+  ]
+}
+
+resource "azurerm_network_interface" "secondaryinterface" {
+  name                = "secondaryinterface"
+  location            = local.location
+  resource_group_name = local.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnetA.id
+    private_ip_address_allocation = "Dynamic"    
+  }
+  depends_on = [
+    azurerm_subnet.subnetA
+  ]
+}
+
+resource "azurerm_managed_disk" "appdisk" {
+  name                = "appdisk"
+  location            = local.location
+  resource_group_name = local.resource_group_name
+  storage_account_type = "Standard_LRS"
+  create_option       = "Empty"
+  disk_size_gb        = "10"
+
+  depends_on = [
     azurerm_resource_group.appgrp
+  ]
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "appdiskattach" {
+  managed_disk_id    = azurerm_managed_disk.appdisk.id
+  virtual_machine_id = azurerm_windows_virtual_machine.appvm.id
+  lun                = "0"
+  caching            = "ReadWrite"
+  depends_on = [
+    azurerm_managed_disk.appdisk,
+    azurerm_windows_virtual_machine.appvm
   ]
 }
--- provider.tf
+++ provider.tf
@@ -1,13 +1,18 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.70.0"
-    }
+  backend "s3" {
+    bucket         = "dev-applications-backend-state-worachai"
+    key            = "dev/azure/backend-state"
+    region         = "us-east-1"
+    dynamodb_table = "dev-applications-locks"
+    encrypt        = true
   }
 }
 
 provider "azurerm" {
   skip_provider_registration = true
   features {}
+}
+
+provider "aws" {
+  region = "us-east-1"  
 }
